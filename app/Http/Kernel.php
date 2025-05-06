9115a11d8fd839d7dd47c7c83db24a95583c7b54
DarkNone20 <jhon.castillo06@usc.edu.co>
2025-05-05 22:36:12 -0500

    Cambio  2.0

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,7 +32,7 @@ class Kernel extends HttpKernel
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\Session\Middleware\StartSession::class, // Asegúrate que esté presente
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
